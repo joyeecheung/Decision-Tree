63b16e2999746aba5a09491fb2835ec9021c785a
Joyee Cheung <joyeec9h3@gmail.com>
2014-12-21 05:03:29 +0800

    smooth learning curve

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@
 from collections import Counter
 
 import matplotlib.pyplot as plt
-from numpy import arange, mean, std
+import numpy as np
+from scipy.interpolate import interp1d
 
 from util import get_filenames
 from preprocess import parse, sample
@@ -19,7 +20,7 @@ def main():
     x, y = [], []
 
     data = list(parse(file(files.dataset)))
-    for test_prop in arange(0.1, 0.99, 0.05):
+    for test_prop in np.arange(0.1, 0.99, 0.05):
         training_set, testing_set = sample(data, test_prop)
         tree = build_tree(training_set).prune(MIN_GAIN)
         check = [record[RESULT_IDX] == plurality(tree.classify(record))
@@ -33,17 +34,24 @@ def main():
         x.append(len(training_set))
         y.append(precision)
 
-    print 'Mean of precision = %.2f' % (mean(y) * 100)
-    print 'Standard deviation of precision = %.2f' % (std(y) * 100)
+    print 'Mean of precision = %.4f' % (np.mean(y))
+    print 'Standard deviation of precision = %.4f' % (np.std(y))
 
     xy = sorted(zip(x, y), key=lambda a: a[0])
     x, y = zip(*xy)
-    plt.plot(x, y)
-    plt.ylim((0.0, 1.0))
+
+    plt.yticks(np.arange(0.0, 1.0, 0.1))
+    plt.ylim(0.0, 1.0)
+    plt.grid(True)
     plt.title('Learning Curve')
     plt.xlabel('Training set size')
-
     plt.ylabel('Precision on test set')
+
+    xnew = np.linspace(np.min(x), np.max(x), 100)
+    ynew = interp1d(x, y)(xnew)
+
+    plt.plot(x, y, '.', xnew, ynew, '--')
+
     plt.savefig(files.curve)
     print 'Save learning curve to', files.curve
 
